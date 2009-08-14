35be6246a3914367c447b6126bed296c619be32e
tristanthomas <tristanthomas@043453e6-25df-4c13-81b0-d6a77d00cc6f>
2009-08-14 17:07:29 +0000

    git-svn-id: https://openseamap.svn.sourceforge.net/svnroot/openseamap/website@9 043453e6-25df-4c13-81b0-d6a77d00cc6f

--- pages/news_en.php
+++ pages/news_en.php
@@ -1,16 +1,15 @@
-Englische News..
-<p><strong>05.08.2009</strong> &nbsp; Der OpenSeaMap Sport-Layer ist online und zeigt vorläufig die Regattabahnen der <a href="http://hanse-sail.webseiten.cc/" target="blank">Hanse Sail</a> an.</p>
-<p><strong>04.08.2009</strong> &nbsp; Dritte Alpha-Version des <a href="http://dev.openseamap.org/sandbox/map_edit.html" target="blank">Online-Editors</a> ist online (Daten werden in OSM-dev gespeichert).</p>
-<p><strong>08.07.2009</strong> &nbsp;  Vorlage "Hafen" in JOSM eingebaut (ab Version 1746).</p>
-<p><strong>07.07.2009</strong> &nbsp;  <a href="http://wiki.openstreetmap.org/wiki/Harbour" target="blank">Hafen-Seite im Wiki</a> nun auch in Englisch.</p>
-<p><strong>26.06.2009</strong> &nbsp;  <a href="http://wiki.openstreetmap.org/wiki/DE:Seamap-Online-Editor" target="blank">Online-Editor hat eine eigene Wikiseite</a>.</p>
-<p><strong>15.06.2009</strong> &nbsp;  <a href="http://wiki.openstreetmap.org/wiki/DE:Hafen" target="blank">Hafen-Seite im Wiki</a> ist fertig.</p>
-<p><strong>18.05.2009</strong> &nbsp; Zweite Alpha-Version des <a href="../map/map_edit.html" target="blank">Online-Editors</a> ist online (Daten werden noch nicht gespeichert).</p>
-<p><strong>13.05.2009</strong> &nbsp; Erste Version einer Karten-Legende ist online.</p>
-<p><strong>09.05.2009</strong> &nbsp; Im <a href="http://www.blog-nestoria.de/" target="blank">Nestoria Blog</a> ist ein <a href="http://www.blog-nestoria.de/nestoria-interview-olaf-hannemann-openseamaporg/" target="blank">Interview mit Olaf Hannemann</a> Über das OpenSeaMap-Projekt veröffentlicht worden.</p>
-<p><strong>07.05.2009</strong> &nbsp; Kieler-Förde wird von OpenSeaMap gerendert.</p>
-<p><strong>01.05.2009</strong> &nbsp; Symbole für Slipanlage, Kran und Hafenmeister werden dargestellt.</p>
-<p><strong>06.04.2009</strong> &nbsp; Sektorenfeuer werden dargestellt.</p>
-<p><strong>05.04.2009</strong> &nbsp; Datenschema korrespondierend zu IHO-S-57 steht.</p>
-<p><strong>03.04.2009</strong> &nbsp; 3-tägiges Entwicklertreffen in Nürnberg.</p>
-<p><strong>20.02.2009</strong> &nbsp; Testhafen "Warnemünde" ist online.</p>
+<p><strong>05.08.2009</strong> &nbsp; The OpenSeaMap sports layer is now enabled which shows race tracks on the map.<a href="http://hanse-sail.webseiten.cc/" target="blank">Hanse Sail</a> an.</p>
+<p><strong>04.08.2009</strong> &nbsp; Third alpha version of<a href="http://dev.openseamap.org/sandbox/map_edit.html" target="blank">the Online-Editor</a> is online (Data is stored to OSeaM-dev).</p>
+<p><strong>08.07.2009</strong> &nbsp;The template "port" was installed at the Guildford evening meet up this week(Version 1746)</p>
+<p><strong>07.07.2009</strong> &nbsp;  <a href="http://wiki.openstreetmap.org/wiki/Harbour" target="blank">The harbour page on the Wiki</a> is now in English as well.</p>
+<p><strong>26.06.2009</strong> &nbsp;  <a href="http://wiki.openstreetmap.org/wiki/Seamap-Online-Editor" target="blank">The online editor has its own Wiki page</a>.</p>
+<p><strong>15.06.2009</strong> &nbsp;  <a href="http://wiki.openstreetmap.org/wiki/DE:Hafen" target="blank">The harbour page on the Wiki</a> is now live(in German).</p>
+<p><strong>18.05.2009</strong> &nbsp; The second alpha version of<a href="../map/map_edit.html" target="blank">the online editor</a> is online (Data is not currently stored to a database).</p>
+<p><strong>13.05.2009</strong> &nbsp; The first version of a map legend is now online</p>
+<p><strong>09.05.2009</strong> &nbsp; In the <a href="http://www.blog-nestoria.de/" target="blank">Nestoria Blog</a> is an<a href="http://www.blog-nestoria.de/nestoria-interview-olaf-hannemann-openseamaporg/" target="blank">interview with Olaf Hannemann</a> about the OpenSeaMap project that has begun.</p>
+<p><strong>07.05.2009</strong> &nbsp; Kieler-Förde is now rendered in OpenSeaMap.</p>
+<p><strong>01.05.2009</strong> &nbsp; Icons for slipways, cranes and harbor masters are now displayed.</p>
+<p><strong>06.04.2009</strong> &nbsp; Fire station is now rendered on the map.</p>
+<p><strong>05.04.2009</strong> &nbsp; Data related to IHO S-57 is rendered.</p>
+<p><strong>03.04.2009</strong> &nbsp; Developer meetup in Nuremberg.</p>
+<p><strong>20.02.2009</strong> &nbsp; Test port "Warnemünde" is online.</p>
