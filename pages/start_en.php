4fd9af28adcb691b4f5010c3d122985209ae799e
tristanthomas <tristanthomas@043453e6-25df-4c13-81b0-d6a77d00cc6f>
2009-08-15 00:31:32 +0000

    git-svn-id: https://openseamap.svn.sourceforge.net/svnroot/openseamap/website@12 043453e6-25df-4c13-81b0-d6a77d00cc6f

--- pages/start_en.php
+++ pages/start_en.php
@@ -1,4 +1,5 @@
-<p>OpenSeaMap's goal is to add nautical and tourism information a sailor could be interested in to OSM and to present it in a pleasing way. Those include beacons, buoys and other seamarks, port information, repair shops, ship supplies and much more, but also shops, restaurants and places of interest.</p>
+<p>Welcome to OpenSeaMap!
+OpenSeaMap's goal is to add nautical and tourism information a sailor could be interested in to OpenStreetMap and to present it in a pleasing and useful way. Those include beacons, buoys and other seamarks, port information, repair shops, ship supplies and much more.  This also includes amenities such as shops, restaurants and places of interest.</p>
 <div id="mapLinks">
 	<a href="map/?lang=<?=$t->getCurrentLanguage()?>"><?=$t->tr("VollbildAnzeigen")?></a>
 </div>
