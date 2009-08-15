854b0b116506e2f0f07a1f00319313a1687c7a69
tristanthomas <tristanthomas@043453e6-25df-4c13-81b0-d6a77d00cc6f>
2009-08-15 00:26:12 +0000

    git-svn-id: https://openseamap.svn.sourceforge.net/svnroot/openseamap/website@10 043453e6-25df-4c13-81b0-d6a77d00cc6f

--- pages/faq_en.php
+++ pages/faq_en.php
@@ -4,7 +4,7 @@
 				</p>
 
 				<p>
-					<strong>Where can I find more information?</strong><br>On the <a href="http://wiki.openstreetmap.org" target="blank">Wiki</a> there is the following information:
+					<strong>Where can I find more information?</strong><br>On the <a href="http://wiki.openstreetmap.org" target="blank">OpenStreetMap Wiki</a> there is more information:
 					<ul>
 						<li><a href="http://wiki.openstreetmap.org/wiki/de:Seekarte" target="blank">Sea Chart(in German)</a></li>
 						<li><a href="http://wiki.openstreetmap.org/wiki/Marine Mapping" target="blank">Marine Mapping</a></li>
@@ -14,16 +14,16 @@
 						<li><a href="http://wiki.openstreetmap.org/wiki/Buoy" target="blank">Buoy</a></li>
 						<li><a href="http://wiki.openstreetmap.org/wiki/Beacon" target="blank">Beacon</a></li>
 					</ul>
-					On each side of the Wiki pages there are further links, including links to Wikipedia, which offers thousands of articles on navigation and cartography specifically for each of the countries mapped in OpenSeaMap.
+					On the sides of the Wiki pages, there are further links, including links to Wikipedia, which offers thousands of articles on navigation and cartography, specific to different countries.
 				</p>
 				<p>
-					<strong>When will the chart cover the whole of the Earth's surface?</strong><br>A completely finished chart will probably never happen, because new things are always being discovered that can be mapped.  However, over the next two months, we hope to render the entire the entire Baltic Sea.  Two months after that, we hope to have the whole of the North Sea rendered.  The remaining oceans are planned to follow at similar intervals.  However, it still wouldn't be complete because we then need to add all the navigation aids.  These will be collected by community members and then entered into the OpenStreetMap database.  You are cordially invited to join in and help!  We need every little contribution.
+					<strong>When will the chart cover the whole of the Earth's surface?</strong><br>A completely finished chart will probably never happen, because new things are always being discovered that can be mapped.  However, over the next two months, we hope to render the entire Baltic Sea.  Two months after that, we hope to have the whole of the North Sea rendered.  The remaining oceans are planned to follow at similar intervals.  However, it still wouldn't be complete because we then need to add all the navigation aids.  These will be collected by community members and then entered into the OpenStreetMap database.  You are cordially invited to join in and help!  We need every little contribution.
 				</p>
 
 				<p>
 					<strong>How can I help?</strong><br>
 					<ul>
-						<li>Are you a <b>seaman</b>, <b>sailor</b>, <b>motorboat pilot</b> or <b>kayaker</b>? <br /><a href="http://wiki.openstreetmap.org/wiki/Marine Mapping"target="blank">You can contribute a great deal using your GPS-device: When you are on a cruise or if you live near a port, please walk along the border of each mole/pier. Then upload the track if you have time and create a port map. Of course you are free to add any information you find interesting, which will then be available to seamen world-wide. You can also begin at home and trace aerial photos of a port, which you can then refine on-site. Every bit of help is valuable for the project.
+						<li>Are you a <b>seaman</b>, <b>sailor</b>, <b>motorboat pilot</b> or <b>kayaker</b>? <br /><a href="http://wiki.openstreetmap.org/wiki/Marine Mapping" target="blank">You can contribute a great deal using your GPS-device</a>: When you are on a cruise or if you live near a port, please walk along the border of each mole/pier. Then upload the track if you have time and create a port map. Of course you are free to add any information you find interesting, which will then be available to seamen world-wide. You can also begin at home and trace aerial photos of a port, which you can then refine on-site. Every bit of help is valuable for the project.
 </li>
 						<li>Do you <b>enjoy tracing aerial photography?</b>? <br />In many parts of the world, highly detailed aerial photography, provided by Yahoo, is available.  From this, exact port plans and detailed coast lines can be drawn and plotted.  You can even use these to dream of places you might be when you are stuck at home....!</li>
 						<li>Are you a <b>programmer</b>? <br />There is a lot of work to do - the Seamap-Editor, the renderer which draws the maps, needs help. The website, the internationalisation, the database, and many supporting programs need programmers. Currently, we also have two places for students writing their theses.<br />To find out more contact: <a href="mailto:ohannemann@opensource-werkstatt-stubben.org?cc=liste12A45q7@gmx.de&amp;subject=Programmierer%20f&uuml;r%20OpenSeaMap">Olaf</a> </li>
@@ -41,6 +41,6 @@
 					<strong>What license is OpenSeaMap under?</strong><br>All the data in the OpenStreetMap database, including OpenSeaMap data, is under the "Creative Commons Attribution Share-Alike 2.0" license.  In short, the license stipulates that any use of OSM data is permissible, even for commercial purposes, however OpenStreetMap must be attributed and the resulting data must be released under the same license.			</p>
 
 				<p>
-					<strong>What is OpenStreetMap?</strong><br><a href="http://www.openstreetmap.org" target="blank">OpenStreetMap</a> is an open source project with the aim of creating a free map of the world.  There are currently more than 120,000 contributors in the project!  Some of them also contribute to OpenSeaMap.  Just as we collect nautical information,  there will bedata on roads, railway, signs, rivers and much more in the database.  We use much of the same data as OpenStreetMap and store all our data in the database.  Because contributors collect the data without using copyrighted sources, we own all the data and hence it can be distributed free.  For more information about OpenStreetMap visit</a>.
+					<strong>What is OpenStreetMap?</strong><br><a href="http://www.openstreetmap.org" target="blank">OpenStreetMap</a> is an open source project with the aim of creating a free map of the world.  There are currently more than 120,000 contributors in the project!  Some of them also contribute to OpenSeaMap.  Just as we collect nautical information,  there will be data on roads, railway, signs, rivers and much more in the database.  We use much of the same data as OpenStreetMap and store all our data in the database.  Because contributors collect the data without using copyrighted sources, we own all the data and hence it can be distributed free.  For more information about OpenStreetMap visit<a href="http://wiki.openstreetmap.org/wiki/openstreetmap" target="blank">here.</a>
 				</p>
 
--- pages/news_en.php
+++ pages/news_en.php
@@ -1,10 +1,10 @@
-<p><strong>05.08.2009</strong> &nbsp; The OpenSeaMap sports layer is now enabled which shows race tracks on the map.<a href="http://hanse-sail.webseiten.cc/" target="blank">Hanse Sail</a> an.</p>
-<p><strong>04.08.2009</strong> &nbsp; Third alpha version of<a href="http://dev.openseamap.org/sandbox/map_edit.html" target="blank">the Online-Editor</a> is online (Data is stored to OSeaM-dev).</p>
+<p><strong>05.08.2009</strong> &nbsp; The OpenSeaMap sports layer is now enabled which shows race tracks on the map from<a href="http://hanse-sail.webseiten.cc/" target="blank">Rostock's Hanse Sail</a>, a festival with more than 230 traditional sailing ships.</p>
+<p><strong>04.08.2009</strong> &nbsp; The third alpha version of <a href="http://dev.openseamap.org/sandbox/map_edit.html" target="blank">the Online Editor</a> is online (Data is stored to OSeaM-dev).</p>
 <p><strong>08.07.2009</strong> &nbsp;The template "port" was installed at the Guildford evening meet up this week(Version 1746)</p>
 <p><strong>07.07.2009</strong> &nbsp;  <a href="http://wiki.openstreetmap.org/wiki/Harbour" target="blank">The harbour page on the Wiki</a> is now in English as well.</p>
 <p><strong>26.06.2009</strong> &nbsp;  <a href="http://wiki.openstreetmap.org/wiki/Seamap-Online-Editor" target="blank">The online editor has its own Wiki page</a>.</p>
 <p><strong>15.06.2009</strong> &nbsp;  <a href="http://wiki.openstreetmap.org/wiki/DE:Hafen" target="blank">The harbour page on the Wiki</a> is now live(in German).</p>
-<p><strong>18.05.2009</strong> &nbsp; The second alpha version of<a href="../map/map_edit.html" target="blank">the online editor</a> is online (Data is not currently stored to a database).</p>
+<p><strong>18.05.2009</strong> &nbsp; The second alpha version of <a href="../map/map_edit.html" target="blank">the online editor</a> is online (Data is not currently stored to a database).</p>
 <p><strong>13.05.2009</strong> &nbsp; The first version of a map legend is now online</p>
 <p><strong>09.05.2009</strong> &nbsp; In the <a href="http://www.blog-nestoria.de/" target="blank">Nestoria Blog</a> is an<a href="http://www.blog-nestoria.de/nestoria-interview-olaf-hannemann-openseamaporg/" target="blank">interview with Olaf Hannemann</a> about the OpenSeaMap project that has begun.</p>
 <p><strong>07.05.2009</strong> &nbsp; Kieler-Förde is now rendered in OpenSeaMap.</p>
