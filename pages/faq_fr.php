b131ef2644594fd5fd77a20cfcc28ab3a45155ff
tristanthomas <tristanthomas@043453e6-25df-4c13-81b0-d6a77d00cc6f>
2009-08-30 15:56:19 +0000

    git-svn-id: https://openseamap.svn.sourceforge.net/svnroot/openseamap/website@51 043453e6-25df-4c13-81b0-d6a77d00cc6f

--- pages/faq_fr.php
+++ pages/faq_fr.php
@@ -18,14 +18,14 @@
 			<p>Une cartographie exhaustive ne verra probablement jamais le jour, car quotidiennement de nouvelles informations peuvent toujours être intégrées. Cependant, le projet -né en Allemagne- entend couvrir l'ensemble de la Mer Baltique dans les deux prochains mois. Puis deux mois après, ce sera au tour de la Mer du Nord d'être développée. Les autres mers et océans sont plannifiés pour être cartographiés à des intervalles similaires. Cependant, ils ne pourront pas être complets car nous aurons besoin de rajouter toutes les aides à la navigation. Ces dernières seont collectées par les membres de la communauté et seront ensuite intégrées dans la base de données OpenStreetMap. Vous être cordialement invités à vous joindre au projet pour apporter votre énergie ! Toute contribution, même minime est la bienvenue.</p>
 			<p><strong>Comment puis-je contribuer?</strong><br>
 			<ul>
-				<li>Etes-vous un <b>navigateur</b>, <b>marin</b>, <b>pêcheur</b>, <b>plaisancier</b> ou même <b>kayakiste</b> ?<br />
+				<li>Etes-vous un <b>navigateur</b>, <b>marin</b>, <b>pêcheur</b>, <b>plaisancier</b> ou même <b>kayakiste</b>?<br />
 					<a href="http://wiki.openstreetmap.org/wiki/Marine Mapping" target="blank">Vous pouvez contribuer de manière significative en utilisant vore récepteur GPS </a>: lorsque vous êtes en croisière ou si vous vivez à proximité d'un port, il suffit de circuler le long de la jetée en enregistrant votre trace. Puis après avoir téléchargé votre trace sauvegardée, vous êtes en mesure de créer une carte portuaire. Naturellement, vous êtes libre d'ajouter toute information que vous estimez intéressante, qui sera ainsi disponible pour tous les navigateurs du monde. Vous pouvez aussi commencer de chez vous en digitalisant les contours du port à partir de photos aériennes, que vous pourrez ensuite affiner localement. Chaque octet d'aide apportée est précieux pour ce projet communautaire.</li>
-				<li>Aimeriez-vous <b>digitaliser des cartes à partir de photographies aériennes</b> .. <br />
+				<li>Aimeriez-vous <b>digitaliser des cartes à partir de photographies aériennes</b>...? <br />
 					Dans de nombreuses régions du monde, des photographies aériennes à haute résolution sont mis à notre disposition par Yahoo!. A partir de ces sources, des plans et des traîts de côte détaillés peuvent être digitalisés. Vous pouvez même travailler sur des endroits rêvés ou futures destinations en restant confortablement installé à la maison...!
-				<li>Etes-vous un <b>programmeur informatique</b> ? <br />
+				<li>Etes-vous un <b>programmeur informatique</b>? <br />
 					Il y a un gros travail à faire : l'éditeur Seamap, le moteur de rendu qui dessine les cartes, a besoin de vous. Le site web, l'internationalisation, la base de données, et des programmes d'assistance ont besoin de programmeurs., Actuellement, nous disposons également de deux places pour des étudiants désirant faire leur thése. <br />
 					Pour en savoir plus, contacter : <a href="mailto:ohannemann@opensource-werkstatt-stubben.org?cc=liste12A45q7@gmx.de,peio@justmagic.com&amp;subject=Programmierer%20f&uuml;r%20OpenSeaMap">Olaf</a>
-				<li>Parlez-vous plusieurs <b>langues</b> ?<br />
+				<li>Parlez-vous plusieurs <b>langues</b>?<br />
 					Nous souhaiterions proposer OpenSeaMap dans le plus de langues possibles. Si vous pouvez traduire le wiki ou le site web dans votre langue, n'hésitez pas à vous signaler.<br />
 					Pour en savoir plus, contacter : <a href="mailto:tristan.thomas@wikinewsie.org">Tristan</a>
 				<li>Etes-vous <b>designer</b> ou <b>graphiste</b> ?<br />
@@ -33,11 +33,11 @@
 					Nous serions heureux de vous en dire plus : <a href="mailto:liste12A45q7@gmx.de?cc=ohannemann@opensource-werkstatt-stubben.org,peio@justmagic.com&amp;subject=Grafiker%20f&uuml;r%20OpenSeaMap">Markus</a>
 				<li>Etes-vous un <b>capitaine</b> de port, un gérant de <b>marina</b>, un <b>armateur</b> de navires ou un opérateur de <b>charter</b> ? Il est probable que vous possédez des données susceptibles d'être intégrées dans le projet OpenSeaMap? Photos aériennes, cartes et plans de port, listes diverses ? Nous pouvons très probablement les utiliser pour le projet !<br />
 					Pour en savoir plus, contacter : <a href="mailto:liste12A45q7@gmx.de?cc=ohannemann@opensource-werkstatt-stubben.org,peio@justmagic.com&amp;subject=&Uuml;bersetzer%20f&uuml;r%20OpenSeaMap">Markus</a>
-				<li>Etes-vous <b>océanographe</b>, <b>cartographe</b>, <b>hydrographe</b> ou <b>météorologiste</b> ?<br />
+				<li>Etes-vous <b>océanographe</b>, <b>cartographe</b>, <b>hydrographe</b> ou <b>météorologiste</b>?<br />
 					Vous avez probablement de nombreuses idées pour améliorer OpenSeaMap. A titre d'exemple, nous recherchons des solutions pour afficher les sondes, isobathes et profils du relief des fonds sous-marins et nous travaillons expérimentalement sur l'AIS.<br />
 					Demander plus d'infos : <a href="mailto:liste12A45q7@gmx.de?cc=ohannemann@opensource-werkstatt-stubben.org,peio@justmagic.com&amp;subject=Daten%20f&uuml;r%20OpenSeaMap">Markus</a></p>
 			</ul>
-			<p><strong>Comment faire pour s'enregisterer ?</strong><br>
+			<p><strong>Comment faire pour s'enregisterer?</strong><br>
 				Pour le moment, cela est assez difficile. L'éditeur en ligne, qui permet d'ajouter facilement des fonctionnalités, est toujours en développement et il est seulement en phase de test alpha. Dans un délai de deux semaines environ, nous espérons rendre possible l'ajout de balises puis d'autres fonctionnalités.</p>
 			<p>
 					<strong>Quelle est la licence sur laquelle repose OpenSeaMap?</strong><br>Toutes les données inclues dans la base de données OpenStreetMap, donc y compris les données OpenSeaMap sont sous la licence &quot;Creative Commons Attribution Share-Alike 2.0&quot;. En bref, cette licence indique que toute utilisation des données OSM est permise, même à des fins commerciales, mais OpenStreetMap doit être stipulé et les données qui en résultent doivent être libérées sous la même licence.</p>
