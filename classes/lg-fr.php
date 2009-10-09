df515e4fe683430be746c7c3b08a650388105c54
al_friede <al_friede@043453e6-25df-4c13-81b0-d6a77d00cc6f>
2009-10-09 13:07:14 +0000

    moved language arrays in extra files
    git-svn-id: https://openseamap.svn.sourceforge.net/svnroot/openseamap/website@100 043453e6-25df-4c13-81b0-d6a77d00cc6f

---
+++ classes/lg-fr.php
@@ -0,0 +1,62 @@
+<?php
+
+$french = array(
+	"langCode"=>"fr",
+	"pageDoesNotExist"=>"Page non trouvée.",
+
+	"dieFreieSeekarte"=>"La cartographie nautique libre",
+
+	// Menu ---------------------------------------------------------
+	"Seekarte"=>"Cartographie nautique",
+	"Vollbild"=>"Plein écran",
+	"VollbildAnzeigen"=>"Voir en plein écran",
+	"SeaChart"=>"Cartographie nautique (plein écran)",
+	"ÜberOpenSeaMap"=>"A propos d'OpenSeaMap",
+	"Impressum"=>"Droits",
+	"Startseite"=>"Page principale",
+	"ÄhnlicheProjekte"=>"Projets similaires",
+	"SomeRights"=>"Ce travail est sous licence Creative Commons Attribution-ShareAlike 2.0",
+
+	// Urls ---------------------------------------------------------
+	"UrlOSM"=>"http://openstreetmap.fr",
+	"UrlOSMWiki_Hauptseite"=>"http://wiki.openstreetmap.org/wiki/Page_Principale",
+	"UrlOSMWiki_OpenSeaMap"=>"http://wiki.openstreetmap.org/wiki/FR:OpenSeaMap",
+
+	// Legende ------------------------------------------------------
+	"Legende"=>"Légende",
+	"Hafen"=>"Port",
+	"Seezeichen"=>"Marques nautiques",
+	"Leuchtfeuer"=>"Feux",
+	"BrückenSchleusen"=>"Ponts/Ecluses",
+	//Harbour
+	"breakwater"=>"Brise-lames",
+	"pier"=>"Jetée",
+	"crane"=>"Grue",
+	"slipway"=>"Cale, Slip",
+	"harbour_master"=>"Capitaine de port",
+	"waste_disposal"=>"Déchetterie",
+	//Seamarks
+	"safe_water"=>"Eaux sécurisées",
+	"lateral_port"=>"Marque bâbord",
+	"lateral_starboard"=>"Marque tribord",
+	"lateral_pref_port"=>"Chenal préféré : bâbord",
+	"lateral_pref_starboard"=>"Chenal préféré : tribord",
+	"cardinal_north"=>"Marque cardinale Nord",
+	"cardinal_east"=>"Marque cardinale Est",
+	"cardinal_south"=>"Marque cardinale Sud",
+	"cardinal_west"=>"Marque cardinale Ouest",
+	"isolated_danger"=>"Marque de danger isolé",
+	"special_purpose"=>"Marque spéciale",
+	//Lights
+	"lighthouse"=>"Phare",
+	"beacon_green"=>"Balise verte",
+	"beacon_red"=>"Balise rouge",
+	"beacon_white"=>"Balise blanche",
+	//Locks
+	"lock_gate"=>"Port de l'écluse",
+	"lock"=>"Ecluse",
+	"wier_small"=>"Défense (petite)",
+	"wier_big"=>"Défense (grosse)",
+);
+
+?>
