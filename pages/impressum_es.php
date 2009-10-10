73fe5f4e7f49bc128774d6f6b0dd18d560ce22ba
markusosm <markusosm@043453e6-25df-4c13-81b0-d6a77d00cc6f>
2009-10-10 15:16:33 +0000

    + Spanisch
    git-svn-id: https://openseamap.svn.sourceforge.net/svnroot/openseamap/website@108 043453e6-25df-4c13-81b0-d6a77d00cc6f

---
+++ pages/impressum_es.php
@@ -0,0 +1,32 @@
+<h2>Apartado legal</h2>
+
+<h4>Proveedor y responsable del contenido de OpenSeaMap </h4>
+
+<pre>
+Olaf Hannemann
+Dorfstraße 17
+23847 Stubben
+</pre>
+
+<p><pre>ohannemann@opensource-werkstatt-stubben.org</pre></p>
+
+<h4>Avisos legales</h4>
+<h5>1. Restricciones</h5>
+
+<p>El contenido de este sitio serán objeto de seguimiento con el mayor cuidado posible, sin embargo el proveedor no garantiza la exactitud y la exhaustividad de los contenidos. El uso de los contenidos de la página web es por cuenta y riesgo de los usuarios. Las contribuciones personales de los colaboradores no reflejan necesariamente las opiniones del autor del sitio o del proveedor.
+</p>
+
+<h5>2. Cartas náuticas oficiales</h5>
+
+<p>Las buenas prácticas marineras requieren del uso de cartas náuticas oficiales. Los mapas de OpenSeaMap son para usar como apoyo a otra cartografía y no pueden sustituir a las cartas náuticas oficiales. Use la cartografía de OpenSeaMap bajo su propio riesgo.
+</p>
+
+<h5>3. Enlaces externos</h5>
+
+<p>Este sitio web contiene enlaces a sitios web de terceros. Estos sitios son propiedad y responsabilidad de sus respectivos propietarios. El proveedor ha comprobado los vínculos que se han añadido, al tiempo que verificaba que no eran sitios dañinos. Sin embargo, el proveedor no tiene control sobre las revisiones actualizadas. La prestación de estos enlaces externos no significa que el proveedor controle el contenido de aquellas o asuma la responsabilidad de sus contenidos. Si se notifica la peligrosidad de algún sitio los enlaces serán eliminados.
+</p>
+
+<h5>3. Traducción</h5>
+
+<p>Nota: Estos textos se han traducido de la versión en inglés y por lo tanto no debe tomarse como completamente fiel al original.
+</p>
