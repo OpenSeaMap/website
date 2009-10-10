1189a08a0e8706e0f9efbf5a371fd22f85170997
markusosm <markusosm@043453e6-25df-4c13-81b0-d6a77d00cc6f>
2009-10-10 15:17:05 +0000

    + Spanisch
    git-svn-id: https://openseamap.svn.sourceforge.net/svnroot/openseamap/website@109 043453e6-25df-4c13-81b0-d6a77d00cc6f

---
+++ pages/news_es.php
@@ -0,0 +1,15 @@
+<p><strong>05.08.2009</strong> &nbsp; La capa de deportes está habilitado, ahora muestra el campo de regatas en el mapa desde <a href="http://hanse-sail.webseiten.cc/" target="blank">Rostock's Hanse Sail</a>.</p>
+<p><strong>04.08.2009</strong> &nbsp; La tercera versión alfa del <a href="http://dev.openseamap.org/sandbox/map_edit.html" target="blank">Editor Online</a> está disponible (los datos son almacenados en OSeaM-dev).</p>
+<p><strong>08.07.2009</strong> &nbsp;  La plantilla "Puerto" está disponible en JOSM (Versión 1746)</p>
+<p><strong>07.07.2009</strong> &nbsp;  <a href="http://wiki.openstreetmap.org/wiki/Harbour" target="blank">La página sobre puertos en la Wiki</a> se encuentra también en inglés.</p>
+<p><strong>26.06.2009</strong> &nbsp;  <a href="http://wiki.openstreetmap.org/wiki/DE:Seamap-Online-Editor" target="blank">El editor online</a>tiene su propia página en la Wiki.</p>
+<p><strong>15.06.2009</strong> &nbsp;  <a href="http://wiki.openstreetmap.org/wiki/DE:Hafen" target="blank">La página sobre puertos en la Wiki</a> está activa.</p>
+<p><strong>18.05.2009</strong> &nbsp; La segunda versión alfa del <a href="../map/map_edit.html" target="blank">online editor</a> está disponible (actualmente los datos no son almacenados en una base de datos).</p>
+<p><strong>13.05.2009</strong> &nbsp; La primera versión de la leyenda del mapa está diponible.</p>
+<p><strong>09.05.2009</strong> &nbsp; En el Blog de Nestoria <a href="http://www.blog-nestoria.de/nestoria-interview-olaf-hannemann-openseamaporg/" target="blank">se entrevista a Olaf Hannemann</a> acerca del proyecto. </p>
+<p><strong>07.05.2009</strong> &nbsp; Kieler-Förde está ahora renderizada.</p>
+<p><strong>01.05.2009</strong> &nbsp; Ahora se muestran iconos para varaderos, grúas y capitanías de puerto.</p>
+<p><strong>06.04.2009</strong> &nbsp; Ahora se representan los parques de bomberos.</p>
+<p><strong>05.04.2009</strong> &nbsp; Se renderizan los datos de la OHI S-57.</p>
+<p><strong>03.04.2009</strong> &nbsp; Reunión de desarrolladores en Nuremberg.</p>
+<p><strong>20.02.2009</strong> &nbsp; El test portuario “Warnemünde" está online.</p>
