a072ab6e84c1a6900d58d2508bfa735ecccbd1d6
markusosm <markusosm@043453e6-25df-4c13-81b0-d6a77d00cc6f>
2009-10-10 15:15:48 +0000

    + Spanisch
    git-svn-id: https://openseamap.svn.sourceforge.net/svnroot/openseamap/website@107 043453e6-25df-4c13-81b0-d6a77d00cc6f

--- pages/faq_en.php
+++ pages/faq_en.php
@@ -1,46 +1,41 @@
 <h2>Frequently Asked Questions</h2>
-				<p>
-					<strong>What is OpenSeaMap?</strong><br>OpenSeaMap is an open source, worldwide project to create a free nautical chart.  There is a great need for freely accessible maps for navigation purposes, so in 2009, OpenSeaMap came into life.  The goal of OpenSeaMap is to record interesting and useful nautical information for the sailor which is then incorporated into a free map of the world.  This includes beacons, buoys and other navigation aids as well as port information, repair shops and chandlerys.  OpenSeaMap is a subproject of OpenStreetMap and uses its database.
-				</p>
+<p><strong>What is OpenSeaMap?</strong><br>OpenSeaMap is an open source, worldwide project to create a free nautical chart. There is a great need for freely accessible maps for navigation purposes, so in 2009, OpenSeaMap came into life. The goal of OpenSeaMap is to record interesting and useful nautical information for the sailor which is then incorporated into a free map of the world. This includes beacons, buoys and other navigation aids as well as port information, repair shops and chandlerys. OpenSeaMap is a subproject of OpenStreetMap and uses its database.
+</p>
 
-				<p>
-					<strong>Where can I find more information?</strong><br>On the <a href="http://wiki.openstreetmap.org" target="blank">OpenStreetMap Wiki</a> there is more information:
-					<ul>
-						<li><a href="http://wiki.openstreetmap.org/wiki/de:Seekarte" target="blank">Sea Chart(in German)</a></li>
-						<li><a href="http://wiki.openstreetmap.org/wiki/Marine Mapping" target="blank">Marine Mapping</a></li>
-						<li><a href="http://wiki.openstreetmap.org/wiki/Harbour" target="blank">Harbour</a></li>
-
-						<li><a href="http://wiki.openstreetmap.org/wiki/Lighthouse" target="blank">Lighthouse</a></li>
-						<li><a href="http://wiki.openstreetmap.org/wiki/Buoy" target="blank">Buoy</a></li>
-						<li><a href="http://wiki.openstreetmap.org/wiki/Beacon" target="blank">Beacon</a></li>
-					</ul>
-					On the sides of the Wiki pages, there are further links, including links to Wikipedia, which offers thousands of articles on navigation and cartography, specific to different countries.
-				</p>
-				<p>
-					<strong>When will the chart cover the whole of the Earth's surface?</strong><br>A completely finished chart will probably never happen, because new things are always being discovered that can be mapped.  However, over the next two months, we hope to render the entire Baltic Sea.  Two months after that, we hope to have the whole of the North Sea rendered.  The remaining oceans are planned to follow at similar intervals.  However, it still wouldn't be complete because we then need to add all the navigation aids.  These will be collected by community members and then entered into the OpenStreetMap database.  You are cordially invited to join in and help!  We need every little contribution.
-				</p>
-
-				<p>
-					<strong>How can I help?</strong><br>
-					<ul>
-						<li>Are you a <b>seaman</b>, <b>sailor</b>, <b>motorboat pilot</b> or <b>kayaker</b>? <br /><a href="http://wiki.openstreetmap.org/wiki/Marine Mapping" target="blank">You can contribute a great deal using your GPS-device</a>: When you are on a cruise or if you live near a port, please walk along the border of each mole/pier. Then upload the track if you have time and create a port map. Of course you are free to add any information you find interesting, which will then be available to seamen world-wide. You can also begin at home and trace aerial photos of a port, which you can then refine on-site. Every bit of help is valuable for the project.
-</li>
-						<li>Do you <b>enjoy tracing aerial photography</b>? <br />In many parts of the world, highly detailed aerial photography, provided by Yahoo, is available.  From this, exact port plans and detailed coast lines can be drawn and plotted.  You can even use these to dream of places you might be when you are stuck at home....!</li>
-						<li>Are you a <b>programmer</b>? <br />There is a lot of work to do - the Seamap-Editor, the renderer which draws the maps, needs help. The website, the internationalisation, the database, and many supporting programs need programmers. Currently, we also have two places for students writing their theses.<br />To find out more contact: <a href="mailto:ohannemann@opensource-werkstatt-stubben.org?cc=liste12A45q7@gmx.de&amp;subject=Programmierer%20f&uuml;r%20OpenSeaMap">Olaf</a> </li>
-						<li>Do you <b>speak languages</b>? <br />We would like to provide OpenSeaMap in as many languages as possible.  If you can translate the wiki or the website into your language, please give us a shout. <br />If you would like to help, contact: <a href="tristan.thomas@wikinewsie.org">Tristan</a> </li>
-						<li>Are you a <b>graphic designer</b>? <br /> A picture tells more than thousand words. You could illustrate our wiki pages, create icons for the map, optimize graphics or layout and organize pictures. <br />We are happy to tell you more: <a href="mailto:liste12A45q7@gmx.de?cc=ohannemann@opensource-werkstatt-stubben.org&amp;subject=Grafiker%20f&uuml;r%20OpenSeaMap">Markus</a> </li>
-
-						<li>Are you a <b>port captain, marina owner, shipowner, charter operator</b>? Maybe you have access to data that OpenSeaMap would be allowed to use? Aerial photos, maps and port plans, lists, tables, indices? We can probably use it somewhere! <br />Contact us for more info: <a href="mailto:liste12A45q7@gmx.de?cc=ohannemann@opensource-werkstatt-stubben.org&amp;subject=Daten%20f&uuml;r%20OpenSeaMap">Markus</a> </li>
-						<li>Are you an <b>oceanographer, cartographer, bathymetrist, meteorologist?</b>? <br />If so, you probably have many ideas for how OpenSeaMap can be improved.  We are currently looking for solutions for displaying water depth, showing a seabed profile and experimenting with AIS. <br />Ask for more information: <a href="mailto:liste12A45q7@gmx.de?cc=ohannemann@opensource-werkstatt-stubben.org&amp;subject=Daten%20f&uuml;r%20OpenSeaMap">Markus</a> </li>					</ul>
-
-				</p>
-				<p>
-					<strong>How do I register?</strong><br>Currently, this is quite difficult.  The online editor, which will make the adding of features easy, is still in development and is only in alpha testing stages.  Within two weeks, we hope to make adding of beacons possible and two weeks later, the adding of other features.
-				</p>
-				<p>
-					<strong>What license is OpenSeaMap under?</strong><br>All the data in the OpenStreetMap database, including OpenSeaMap data, is under the "Creative Commons Attribution Share-Alike 2.0" license.  In short, the license stipulates that any use of OSM data is permissible, even for commercial purposes, however OpenStreetMap must be attributed and the resulting data must be released under the same license.			</p>
-
-				<p>
-					<strong>What is OpenStreetMap?</strong><br><a href="http://www.openstreetmap.org" target="blank">OpenStreetMap</a> is an open source project with the aim of creating a free map of the world.  There are currently more than 120,000 contributors in the project!  Some of them also contribute to OpenSeaMap.  Just as we collect nautical information,  there will be data on roads, railway, signs, rivers and much more in the database.  We use much of the same data as OpenStreetMap and store all our data in the database.  Because contributors collect the data without using copyrighted sources, we own all the data and hence it can be distributed free.  For more information about OpenStreetMap visit<a href="http://wiki.openstreetmap.org/wiki/openstreetmap" target="blank"> here.</a>
-				</p>
-
+<p><strong>Where can I find more information?</strong><br>On the <a href="http://wiki.openstreetmap.org" target="blank">OpenStreetMap Wiki</a> there is more information:
+<ul>
+<li><a href="http://wiki.openstreetmap.org/wiki/de:Seekarte" target="blank">Sea Chart (in German)</a></li>
+<li><a href="http://wiki.openstreetmap.org/wiki/Marine Mapping" target="blank">Marine Mapping</a></li>
+<li><a href="http://wiki.openstreetmap.org/wiki/Harbour" target="blank">Harbour</a></li>
+<li><a href="http://wiki.openstreetmap.org/wiki/Lighthouse" target="blank">Lighthouse</a></li>
+<li><a href="http://wiki.openstreetmap.org/wiki/Buoy" target="blank">Buoy</a></li>
+<li><a href="http://wiki.openstreetmap.org/wiki/Beacon" target="blank">Beacon</a></li>
+</ul>
+On the sides of the Wiki pages, there are further links, including links to Wikipedia, which offers thousands of articles on navigation and cartography, specific to different countries.
+</p>
+<p><strong>When will the chart cover the whole of the Earth's surface?</strong><br>A completely finished chart will probably never happen, because new things are always being discovered that can be mapped.  However, over the next two months, we hope to render the entire Baltic Sea.  Two months after that, we hope to have the whole of the North Sea rendered.  The remaining oceans are planned to follow at similar intervals.  However, it still wouldn't be complete because we then need to add all the navigation aids.  These will be collected by community members and then entered into the OpenStreetMap database.  You are cordially invited to join in and help!  We need every little contribution.
+</p>
+<p><strong>How can I help?</strong><br>
+	<ul>
+		<li>Are you a <b>seaman</b>, <b>sailor</b>, <b>motorboat pilot</b> or <b>kayaker</b>? <br /><a href="http://wiki.openstreetmap.org/wiki/Marine Mapping" target="blank">You can contribute a great deal using your GPS-device</a>: When you are on a cruise or if you live near a port, please walk along the border of each mole/pier. Then upload the track if you have time and create a port map. Of course you are free to add any information you find interesting, which will then be available to seamen world-wide. You can also begin at home and trace aerial photos of a port, which you can then refine on-site. Every bit of help is valuable for the project.
+		</li>
+		<li>Do you <b>enjoy tracing aerial photography</b>? <br />In many parts of the world, highly detailed aerial photography, provided by Yahoo, is available.  From this, exact port plans and detailed coast lines can be drawn and plotted.  You can even use these to dream of places you might be when you are stuck at home....!
+		</li>
+		<li>Are you a <b>programmer</b>? <br />There is a lot of work to do - the Seamap-Editor, the renderer which draws the maps, needs help. The website, the internationalisation, the database, and many supporting programs need programmers. Currently, we also have two places for students writing their theses.<br />To find out more contact: <a href="mailto:ohannemann@opensource-werkstatt-stubben.org?cc=liste12A45q7@gmx.de&amp;subject=Programmierer%20f&uuml;r%20OpenSeaMap">Olaf</a>
+		</li>
+		<li>Do you <b>speak languages</b>? <br />We would like to provide OpenSeaMap in as many languages as possible.  If you can translate the wiki or the website into your language, please give us a shout. <br />If you would like to help, contact: <a href="tristan.thomas@wikinewsie.org">Tristan</a> 
+		</li>
+		<li>Are you a <b>graphic designer</b>? <br /> A picture tells more than thousand words. You could illustrate our wiki pages, create icons for the map, optimize graphics or layout and organize pictures. <br />We are happy to tell you more: <a href="mailto:liste12A45q7@gmx.de?cc=ohannemann@opensource-werkstatt-stubben.org&amp;subject=Grafiker%20f&uuml;r%20OpenSeaMap">Markus</a> 
+		</li>
+		<li>Are you a <b>port captain, marina owner, shipowner, charter operator</b>? Maybe you have access to data that OpenSeaMap would be allowed to use? Aerial photos, maps and port plans, lists, tables, indices? We can probably use it somewhere! <br />Contact us for more info: <a href="mailto:liste12A45q7@gmx.de?cc=ohannemann@opensource-werkstatt-stubben.org&amp;subject=Daten%20f&uuml;r%20OpenSeaMap">Markus</a> 
+		</li>
+		<li>Are you an <b>oceanographer, cartographer, bathymetrist, meteorologist?</b>? <br />If so, you probably have many ideas for how OpenSeaMap can be improved.  We are currently looking for solutions for displaying water depth, showing a seabed profile and experimenting with AIS. <br />Ask for more information: <a href="mailto:liste12A45q7@gmx.de?cc=ohannemann@opensource-werkstatt-stubben.org&amp;subject=Daten%20f&uuml;r%20OpenSeaMap">Markus</a> 
+		</li>
+	</ul>
+</p>
+<p><strong>How do I register?</strong><br>Currently, this is quite difficult.  The online editor, which will make the adding of features easy, is still in development and is only in alpha testing stages.  Within two weeks, we hope to make adding of beacons possible and two weeks later, the adding of other features.
+</p>
+<p><strong>What license is OpenSeaMap under?</strong><br>All the data in the OpenStreetMap database, including OpenSeaMap data, is under the "Creative Commons Attribution Share-Alike 2.0" license. In short, the license stipulates that any use of OSM data is permissible, even for commercial purposes, however OpenStreetMap must be attributed and the resulting data must be released under the same license. 
+</p>
+<p><strong>What is OpenStreetMap?</strong><br><a href="http://www.openstreetmap.org" target="blank">OpenStreetMap</a> is an open source project with the aim of creating a free map of the world.  There are currently more than 120,000 contributors in the project! Some of them also contribute to OpenSeaMap. Just as we collect nautical information, there will be data on roads, railway, signs, rivers and much more in the database. We use much of the same data as OpenStreetMap and store all our data in the database. Because contributors collect the data without using copyrighted sources, we own all the data and hence it can be distributed free. For more information about OpenStreetMap visit the <a href="http://wiki.openstreetmap.org/wiki/openstreetmap" target="blank">OSM-Wiki</a>.
+</p>
