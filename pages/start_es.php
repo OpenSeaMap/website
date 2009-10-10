3529a4c50f45df5ac5583276e2afa3dea94e0ddd
markusosm <markusosm@043453e6-25df-4c13-81b0-d6a77d00cc6f>
2009-10-10 15:17:47 +0000

    + Spanisch
    git-svn-id: https://openseamap.svn.sourceforge.net/svnroot/openseamap/website@110 043453e6-25df-4c13-81b0-d6a77d00cc6f

---
+++ pages/start_es.php
@@ -0,0 +1,7 @@
+<p>¡Bienvenido a OpenSeaMap!
+El objetivo de OpenSeaMap! es añadir información sobre turismo náutico-recreativo para marineros interesados en OpenStreetMap y presentarla de manera agradable y útil. Así, se incluye luces marítimas, boyas y otras señales de navegación, información portuaria, talleres de reparación, suministros para embarcaciones y mucho más. También se incluyen otros servicios asociados tales como tiendas, restaurantes y lugares de interés.</p>
+<div id="mapLinks">
+	<a href="map/?lang=<?=$t->getCurrentLanguage()?>"><?=$t->tr("VollbildAnzeigen")?></a>
+</div>
+<div id="map"></div>
+
