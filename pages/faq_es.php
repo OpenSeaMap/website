7eb1765cbefa76ab9b085da6e355fb2e9e2a0c5d
markusosm <markusosm@043453e6-25df-4c13-81b0-d6a77d00cc6f>
2009-10-10 15:20:12 +0000

    + Spanisch
    git-svn-id: https://openseamap.svn.sourceforge.net/svnroot/openseamap/website@112 043453e6-25df-4c13-81b0-d6a77d00cc6f

---
+++ pages/faq_es.php
@@ -0,0 +1,52 @@
+<h2>Preguntas frecuentes</h2>
+<p>
+	<strong>¿Qué es OpenSeaMap?</strong><br>OpenSeaMap es un proyecto de código abierto para crear una carta náutica libre y gratuita a nivel mundial. Existe una gran necesidad de mapas de libre disposición para la navegación., es por ello que en el año 2009 OpenSeaMap inició su andadura. El objetivo del proyecto OpenSeaMap es registrar toda aquella información náutica interesante y útil para el marinero, e incorporarla a un mapa gratuito del mundo. Esta cartografía incluye información sobre luces, boyas y otras ayudas a la navegación, así como información sobre los puertos, talleres de reparación y tiendas de efectos navales. Openstreetmap es un subproyecto de <a href="http://www.openstreetmap.de/index.html" target="blank">OpenStreetMap</a> y utiliza su base de datos.
+</p>
+<p>
+	<strong>¿Dónde puedo encontrar más información?</strong><br>En la <a href="http://wiki.openstreetmap.org/index.php/es:Main_Page" target="blank">Wiki</a> existe más información sobre:
+	<ul>
+		<li><a href="http://wiki.openstreetmap.org/wiki/de:Seekarte" target="blank">mapa marítimo
+ (en alemán )</a></li>
+		<li><a href="http://wiki.openstreetmap.org/wiki/Marine_Mapping" target="blank">Cartografía marina</a></li>
+		<li><a href="http://wiki.openstreetmap.org/wiki/Harbour" target="blank">Puerto</a></li>
+		<li><a href="http://wiki.openstreetmap.org/wiki/Lighthouse" target="blank">Faros</a></li>
+		<li><a href="http://wiki.openstreetmap.org/wiki/Buoy" target="blank">Boyas</a></li>
+		<li><a href="http://wiki.openstreetmap.org/wiki/Beacon" target="blank">Balizas</a></li>
+	</ul>
+	Junto a las páginas de la wiki existen otros enlaces entre los que se incluyen vínculos a la Wikipedia, la cual ofrece miles de artículos sobre navegación y cartografía, específicas para diferentes países que se muestran en el mapa.
+</p>
+<p>
+	<strong>¿Cuándo la carta náutica cubrirá el conjunto de la superficie terrestre?</strong><br>Lo más probable es que nunca se llegue a obtener una carta náutica completamente terminada ya que siempre existirá nueva información que agregar al mapa. No obstante, durante los próximos dos meses esperamos tener hecho todo el Mar Báltico. Dos meses después esperamos contar con todo el Mar del Norte completado. El resto de océanos está previsto que lleve intervalos de tiempo similares. Sin embargo, todavía no estaría completado porque se necesitaría añadir información útil para ayudar a la navegación. Estos datos serán recopilados por los miembros de la comunidad y serán añadidos a la base de datos de OpenStreetMap. ¡Te invitamos cordialmente a unirte a nosotros y ayudar! Necesitamos cualquier pequeña contribución.
+</p>
+<p>
+	<strong>¿Cómo puedo ayudar?</strong><br>
+	<ul>
+		<li>¿Eres <b>marinero</b>, <b>navegante</b>, <b>patrón de embarcación</b> o <b>aficionado al kayak de travesía</b>? <br /><a href="http://wiki.openstreetmap.org/wiki/de:Segler"target="blank">Puedes hacer una gran contribución a través de tu dispositivo GPS</a>: Cuando estás embarcado o si vives cerca de un puerto, por favor, camina a lo largo de los límites de los muelles o diques. A continuación, sube tu traza, y si tienes tiempo dibuja el  <a href="http://wiki.openstreetmap.org/wiki/Harbour" target="blank">mapa de tu puerto</a>. Por supuesto, eres libre de añadir cualquier información que te parezca interesante, la cual luego estará disponible para los aficionados a la mar de todo el mundo.
+Son muy valiosos para el proyecto esos "pequeños" datos de un determinado puerto que ya conoces. Por supuesto puedes señalar errores que has identificado en un lugar o información que se podría mejorar. También puedes en tu casa localizar y dibujar los puertos sobre fotografías aéreas, y detallarlo posteriormente si vas a visitarlos al lugar. Toda esa información es muy importante  para el proyecto.
+		</li>
+		<li>¿Disfrutas <b>dibujando sobre fotografía aérea</b>? <br />Existen fotos aéreas de gran detalle de muchos lugares del mundo facilitadas por Yahoo, las cuales ya están disponibles. Esto permite digitalizar de forma precisa las líneas de costa o de, por ejemplo, idílicos lugares desde el salón de tu casa.
+		</li>
+		<li>¿Es usted un <b>programador</b>? <br />Hay mucho trabajo por hacer - el <a href="http://wiki.openstreetmap.org/wiki/de:Seamap-Editor"target="blank">Editor SEAMAP (en alemán)</a>, el renderizador que dibuja los mapas, etc. necesita de ayuda. El sitio web, la internacionalización, la base de datos, y muchos programas de apoyo requieren de programadores. En la actualidad tenemos también dos plazas para estudiantes que deseen escribir su tesis.
+<br />Para más información contactar con: <a href="mailto:ohannemann@opensource-werkstatt-stubben.org?cc=liste12A45q7@gmx.de&amp;subject=Programmierer%20f&uuml;r%20OpenSeaMap">Olaf</a> 		</li>
+		<li>¿Habla <b>idiomas</b>? <br />Nos gustaría ofrecer OpenSeaMap en tantos idiomas como sea posible. Si puedes traducir la wiki o el sitio web a tu idioma, por favor avísanos
+<br />Si deseas ayudar, comunícate con: <a href="mailto:tristan.thomas@wikinewsie.org">Tristan</a> </li>
+		<li>¿Eres <b>diseñador gráfico</b>? <br />Una imagen dice más que mil palabras. Puedes ilustrar nuestras páginas de la wiki, crear iconos para el mapa, optimizar gráficos o diseñar y organizar imágenes.
+<br />Estamos encantados de darte más información: <a href="mailto:liste12A45q7@gmx.de?cc=ohannemann@opensource-werkstatt-stubben.org&amp;subject=Grafiker%20f&uuml;r%20OpenSeaMap">Markus</a>
+		</li>
+		<li>¿Eres <b>capitán de puerto, propietario de embarcación, armador, fletador</b>? ¿Quizás tengas acceso a datos que se puedan usar en OpenSeaMap: fotos aéreas, cartas náuticas y planos de puertos, listados, tablas, índices?
+¡Probablemente podamos usarlo! 
+<br />Contacta con nosotros para más información: <a href="mailto:liste12A45q7@gmx.de?cc=ohannemann@opensource-werkstatt-stubben.org&amp;subject=Daten%20f&uuml;r%20OpenSeaMap">Markus</a> </li>
+		<li>Du bist <b>oceanógrafo, cartógrafo, experto en batimetrías, meteorólogo</b>? <br />Si es así probablemente tengas muchas ideas de cómo mejorar OpenSeaMap. Actualmente estamos buscando soluciones para mostrar <a href="http://wiki.openstreetmap.org/wiki/Water_Depth"target="blank">la profundidad marina</a>, que muestre el <a href="http://wiki.openstreetmap.org/wiki/Bathymetry"target="blank">perfil de los fondos marinos</a> que muestre el y experimentar con<a href="http://wiki.openstreetmap.org/wiki/AIS"target="blank">AIS</a>. <br />Solicitar más información: <a href="mailto:liste12A45q7@gmx.de?cc=ohannemann@opensource-werkstatt-stubben.org&amp;subject=Daten%20f&uuml;r%20OpenSeaMap">Markus</a>
+		</li>
+	</ul>
+</p>
+<p>
+	<strong>¿Cómo puedo añadir una baliza al mapa?</strong><br>En la actualidad, esto es bastante difícil. El editor en línea, que hará que la agregación de información sea más fácil, todavía está en desarrollo y se encuentra en las fases alfa de prueba. Dentro de dos semanas esperamos que se puedan añadir balizas y dos semanas más tarde agregar otras características.
+</p>
+<p>
+	<strong>¿Bajo que licencia se encuentra OpenSeaMap?</strong><br>Todos los datos de la base de datos de OpenStreetMap, incluidos los datos de OpenSeaMap, está bajo la licencia <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="blank">""Creative Commons Reconocimiento-Compartir Igual 2.0""</a>). En definitiva, esta licencia estipula que cualquiera puede hacer uso de los datos de OSM, incluso para fines comerciales, sin embargo deben atribuirse la autoría a la Fundación OpenStreetMap y sus contribuidores, y los datos resultantes deben ser liberados bajo la misma licencia.
+</p>
+
+<p>
+	<strong>¿Qué es OpenStreetMap?</strong><br><a href="http://www.openstreetmap.org" target="blank">OpenStreetMap</a> es un proyecto de fuente abierta cuyo objetivo es crear un mapa libre y gratuito del mundo. ¡En este momento hay más de 150.000 colaboradores en el proyecto! Algunos de ellos también contribuyen en OpenSeaMap. Así como nosotros recogemos información de interés náutico, otros recogen datos sobre carreteras, ferrocarriles, señales de tráfico, ríos y mucho más y los agregan en la base de datos común. Nosotros utilizamos la mayor parte de los mismos datos que OpenStreetMap y almacenamos toda nuestra información que recopilamos en la misma base de datos. Debido a que los contribuyentes reúnen estos datos sin necesidad de utilizar fuentes de derechos de autor, somos propietarios de esos datos y por lo tanto pueden ser distribuidos gratuitamente bajo licencia abierta. Para obtener más información sobre la OpenStreetMap visita <a href="[http://wiki.openstreetmap.org/wiki/es:FAQ" target="blank">OpenStreetMap-FAQ</a>.
+</p>
