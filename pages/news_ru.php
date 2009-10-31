efa80a5b1d2a59fa2d6e2fcd5778b40339b670ee
markusosm <markusosm@043453e6-25df-4c13-81b0-d6a77d00cc6f>
2009-10-31 22:07:04 +0000

    Russische Übersetzung
    git-svn-id: https://openseamap.svn.sourceforge.net/svnroot/openseamap/website@139 043453e6-25df-4c13-81b0-d6a77d00cc6f

---
+++ pages/news_ru.php
@@ -0,0 +1,15 @@
+<p><strong>05.08.2009</strong> &nbsp; Добавлен спортивный слой - на нём показываются маршруты <a href="http://hanse-sail.webseiten.cc/" target="blank">Rostock's Hanse Sail</a>.</p>
+<p><strong>04.08.2009</strong> &nbsp; Выпущена третья альфа-версия <a href="http://dev.openseamap.org/sandbox/map_edit.html" target="blank">онлайн-редактора</a> (данные сохраняются в базу OSeaM-dev).</p>
+<p><strong>08.07.2009</strong> &nbsp; В JOSM добавлен шаблон «порт» (в версии 1746)</p>
+<p><strong>07.07.2009</strong> &nbsp; В вики страничка про <a href="http://wiki.openstreetmap.org/wiki/Harbour" target="blank">гавани</a> переведена на английский.</p>
+<p><strong>26.06.2009</strong> &nbsp; У нашего <a href="http://wiki.openstreetmap.org/wiki/DE:Seamap-Editor" target="blank">онлайн-редактора</a> появилась собственная страничка в вики..</p>
+<p><strong>15.06.2009</strong> &nbsp; В вики добавлено описание <a href="http://wiki.openstreetmap.org/wiki/DE:Hafen" target="blank">гавани</a> (пока только на немецком).</p>
+<p><strong>18.05.2009</strong> &nbsp; Выпущена вторая альфа-версия <a href="../map/map_edit.html" target="blank">онлайн-редактора</a> (данные пока не пишутся в базу)..</p>
+<p><strong>13.05.2009</strong> &nbsp; Выпущена первая версия условных обозначений.</p>
+<p><strong>09.05.2009</strong> &nbsp; <a href="http://www.blog-nestoria.de/nestoria-interview-olaf-hannemann-openseamaporg/" target="blank">Интервью с Олафом Ханнеманом</a> о проекте в блоге <a href="http://www.blog-nestoria.de/" target="blank">Nestoria</a>.</p>
+<p><strong>07.05.2009</strong> &nbsp; Отрендерена карта Кильского залива.</p>
+<p><strong>01.05.2009</strong> &nbsp; Начали отображаться значки стапелей, кранов и портовых офисов.</p>
+<p><strong>06.04.2009</strong> &nbsp; Начали отображаться пожарные станции.</p>
+<p><strong>05.04.2009</strong> &nbsp; Отображаются данные, описанные в стандарте IHO S-57.</p>
+<p><strong>03.04.2009</strong> &nbsp; Встреча разработчиков в Нюрнберге.</p>
+<p><strong>20.02.2009</strong> &nbsp; Запущена тестовая карта Варнемюнде.</p>
