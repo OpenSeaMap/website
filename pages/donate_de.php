73982fb8bff2ce6d3504fb951d508eab453288b1
markusosm <markusosm@043453e6-25df-4c13-81b0-d6a77d00cc6f>
2009-11-27 21:16:27 +0000

    git-svn-id: https://openseamap.svn.sourceforge.net/svnroot/openseamap/website@163 043453e6-25df-4c13-81b0-d6a77d00cc6f

---
+++ pages/donate_de.php
@@ -0,0 +1,46 @@
+<h2>Spenden</h2>
+
+<h4>OpenSeaMap braucht Unterstützung</h4>
+
+<p>Für die Durchführung der "boot" in Düsseldorf kommen viele Kosten auf uns zu (Messestand, Gebühren, Reise, Unterkunft und Verpflegung für 10 Tage). Wir freuen und über jeden Euro, den wir nicht aus eigener Tasche bezahlen müssen!</p>
+
+<p>Wir brauchen dringend einen "Rechenknecht", der weltweit Kacheln rechnet</p>
+
+<h4>Spendenkonto</h4>
+
+<p>Der FOSSGIS e.V. übernimmt für OpenSeaMap (wie auch für Openstreetmap) die Verwaltung von Spendengeldern und stellt steuerlich absetzungsfähige Spendenbescheinigungen aus. Bitte schreiben Sie mir, ich sende Ihnen gern eine Spendenbescheinigung: <a href="mailto:liste12A45q7@gmx.de?cc=ohannemann@opensource-werkstatt-stubben.org&amp;subject=Daten%20f&uuml;r%20OpenSeaMap">Markus</a></p>
+
+<ul>
+<table border=0>
+<tr>
+	<td>Empfänger: </td>
+	<td>OpenSeaMap</td>
+</tr>
+<tr>
+	<td>Kontoinhaber: </td>
+	<td>FOSSGIS e.V.
+</tr>
+<tr>
+	<td>Kontonummer: </td>
+	<td>415938018</td>
+</tr>
+<tr>
+	<td>BLZ: </td>
+	<td>551 900 00 Mainzer Volksbank</td>
+</tr>
+</table>
+</ul>
+
+<h4>Sachspenden</h4>
+
+<p>Den aktuellen Hardware-Bedarf beschreibt Ihnen gern <a href="mailto:ohannemann@opensource-werkstatt-stubben.org?cc=liste12A45q7@gmx.de&amp;subject=Programmierer%20f&uuml;r%20OpenSeaMap">Olaf</a> <br>Über alle anderen Angebote freut sich <a href="mailto:liste12A45q7@gmx.de?cc=ohannemann@opensource-werkstatt-stubben.org&amp;subject=Daten%20f&uuml;r%20OpenSeaMap">Markus</a></p>
+
+<p>Bedarf haben wir für: </p>
+<ol>
+<li>Videoproduktion<li>
+<li>Textildruck</li>
+<li>Kartendruck</li>
+<li>Bannerdruck</li>
+<li>CD/DVD/Speicherkarten und Datenkopieren
+<li>GPS-Testgeräte, Kartenplotter</li>
+</ol>
